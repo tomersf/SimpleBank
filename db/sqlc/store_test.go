0d30d97e990f2db6d37d7d289c52f9643d111609
Tomer Shafir <tomer1up@gmail.com>
2023-04-17 15:58:34 +0300

    improve sql queries to prevent deadlock, add new query for add balance

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,15 +13,16 @@ func TestTransferTx(t *testing.T) {
 
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
 
 	// run n concurrent transfer transactions
-	n := 5
+	numOfTransactions := 5
 	amount := int64(10)
 
 	errs := make(chan error)
 	results := make(chan TransferTxResult)
 
-	for i := 0; i < n; i++ {
+	for i := 0; i < numOfTransactions; i++ {
 		go func() {
 			result, err := store.TransferTx(context.Background(), TransferTxParams{
 				FromAccountID: account1.ID,
@@ -34,7 +36,8 @@ func TestTransferTx(t *testing.T) {
 	}
 
 	// check results
-	for i := 0; i < n; i++ {
+	existed := make(map[int]bool)
+	for i := 0; i < numOfTransactions; i++ {
 		err := <-errs
 		require.NoError(t, err)
 
@@ -74,6 +77,38 @@ func TestTransferTx(t *testing.T) {
 		_, err = store.GetEntry(context.Background(), toEntry.ID)
 		require.NoError(t, err)
 
-		// TODO: check accounts' balance
+		// check accounts
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		// check accounts balance
+		fmt.Println(">> tx:", fromAccount.Balance, toAccount.Balance)
+		diff1 := account1.Balance - fromAccount.Balance
+		diff2 := toAccount.Balance - account2.Balance
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1%amount == 0) //
+
+		transactionIndex := int(diff1 / amount) // transactionIndex should be 1 for 1st transaction, 2 for 2nd and so on, up to numOfTransactions
+		require.True(t, transactionIndex >= 1 && transactionIndex <= numOfTransactions)
+		require.NotContains(t, existed, transactionIndex)
+
+		existed[transactionIndex] = true
 	}
+
+	// check the final updated balances
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+	require.Equal(t, account1.Balance-(int64(numOfTransactions)*amount), updatedAccount1.Balance)
+	require.Equal(t, account2.Balance+(int64(numOfTransactions)*amount), updatedAccount2.Balance)
 }
