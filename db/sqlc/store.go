df5c86c9ed5f48d358c402ae04af7f51ddba7f3d
Tomer Shafir <tomer1up@gmail.com>
2023-04-19 21:17:19 +0300

    add go mock and getAccount api mock tests

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -7,21 +7,27 @@ import (
 )
 
 // Store provides all functions to execute db queries and transactions.
-type Store struct {
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+// SQLStore provides all functions to execute SQL queries and transactions.
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
 // Create a new Store
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 // execTx executes a fn within a database transaction
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
@@ -57,7 +63,7 @@ type TransferTxResult struct {
 
 // Performs a money transfer from one account to another
 // Creates a transfer record, add account entries, update account balances all within a   single db transaction.
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.execTx(ctx, func(q *Queries) error {
